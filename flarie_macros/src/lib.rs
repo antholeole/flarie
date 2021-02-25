6a46b30a15e175847f4d4a3e56516498cad105f9
antholeole <antholeinik@gmail.com>
2021-02-24 22:39:14 -0600

    gut pt 2

--- flarie_macros/src/lib.rs
+++ flarie_macros/src/lib.rs
@@ -8,24 +8,4 @@ mod path_params;
 use proc_macro::TokenStream;
 
 use syn::{parse_macro_input, AttributeArgs, ItemFn};
-
-macro_rules! build_methods {
-    ($curr:ident, $($rest:tt)+) => {
-        build_methods!($curr);
-        build_methods!($($rest)*);
-    };
-
-    ($curr:ident) => {
-        #[proc_macro_attribute]
-        pub fn $curr(metadata: TokenStream, input: TokenStream) -> TokenStream {
-            let metadata = parse_macro_input!(metadata as AttributeArgs);
-            let input = parse_macro_input!(input as ItemFn);
-
-            create_fn::parse_route_fn(input, metadata, stringify!($curr))
-                .unwrap_or_else(|err| err.to_compile_error())
-                .into()
-        }
-    };
-}
-
-build_methods!(get, post, delete, put, patch);
+\
